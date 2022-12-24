2987f246d1f7f86a3498f87fd165e180784bc8c4
gerrickb <27402037+GerrickB@users.noreply.github.com>
2022-12-24 02:43:06 -0800

    Test File.readlines to read line by line

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,10 @@
 puts 'Hangman initialized.'
 
 p File.exists? "google-10000-english-no-swears.txt"
-contents = File.read('google-10000-english-no-swears.txt')
+#contents = File.read('google-10000-english-no-swears.txt')
 #puts contents
+
+lines = File.readlines('google-10000-english-no-swears.txt')
+lines.each do |line|
+  puts line
+end
