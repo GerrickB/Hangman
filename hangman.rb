7b6e3c39e7a75a160d28207233d6ebe33f262ece
gerrickb <27402037+GerrickB@users.noreply.github.com>
2022-12-24 11:25:32 -0800

    Add code to randomize word choice

--- hangman.rb
+++ hangman.rb
@@ -1,10 +1,28 @@
 puts 'Hangman initialized.'
 
 p File.exists? "google-10000-english-no-swears.txt"
-#contents = File.read('google-10000-english-no-swears.txt')
-#puts contents
+# Creates a string of the whole file
+contents = File.read('google-10000-english-no-swears.txt')
 
+# Creates an array of each lines
 lines = File.readlines('google-10000-english-no-swears.txt')
-lines.each do |line|
-  puts line
+
+p contents.class
+p lines.class
+p lines[0]
+p lines[0].chomp
+p lines.length
+
+# When a new game is started, your script should load in the dictionary 
+# and randomly select a word between 5 and 12 characters long for the secret word.
+#prng = Random.new
+word = ''
+prng = Random.rand(lines.length)
+
+loop do
+  prng = Random.rand(lines.length)
+  word = lines[prng].chomp
+  break if word.length >= 5 and word.length <= 12
 end
+
+p word
