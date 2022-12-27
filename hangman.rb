cbdd93842dd712170a6df88b44083edcf4d01561
gerrickb <27402037+GerrickB@users.noreply.github.com>
2022-12-27 02:13:10 -0800

    Move word randomizer to a method

--- hangman.rb
+++ hangman.rb
@@ -6,7 +6,7 @@ p File.exists? "google-10000-english-no-swears.txt"
 # p contents.class
 
 # Creates an array of each lines
-lines = File.readlines('google-10000-english-no-swears.txt')
+file_array = File.readlines('google-10000-english-no-swears.txt')
 
 # For checking
 def test_file(file)
@@ -18,25 +18,26 @@ end
 
 # When a new game is started, your script should load in the dictionary 
 # and randomly select a word between 5 and 12 characters long for the secret word.
-#prng = Random.new
-word = ''
-prng = Random.rand(lines.length)
 
-loop do
-  prng = Random.rand(lines.length)
-  word = lines[prng].chomp
-  break if word.length >= 5 and word.length <= 12
+def select_random_word(file)
+  word = ''
+  loop do
+    random_index = Random.rand(file.length)
+    word = file[random_index].chomp
+    break if word.length >= 5 and word.length <= 12
+  end
+  word
 end
 
-p word
+p select_random_word(file_array)
 
 # blank array to hold ( _ _ _ _ ), no space needed
 array = []
 
 # pushes _ based on the word's length
-word.length.times do 
-  array.push('_')
-end
+#word.length.times do 
+  #array.push('_')
+#end
 
 p array
 
